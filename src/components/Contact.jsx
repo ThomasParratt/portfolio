8c09fdc6268cd4cb0666879a890698d0a2b18ce4
ThomasParratt <thomasparratt@googlemail.com>
2026-01-20 11:22:40 +0200

    contact button colour

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
                     <form className="mt-4 flex flex-col gap-3" onSubmit={onSubmit}>
                         <input required value={email} onChange={e=>setEmail(e.target.value)} placeholder="Your email" className="px-4 py-2 border rounded" />
                         <textarea required value={message} onChange={e=>setMessage(e.target.value)} placeholder="Message" className="px-4 py-2 border rounded h-32" />
-                        <button className="w-max px-4 py-2 bg-gray-900 text-white rounded">Send</button>
+                        <button className="w-max px-4 py-2 bg-gray-900 text-white rounded bg-sky-700">Send</button>
                     </form>
                 )}
             </div>

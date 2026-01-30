a68bb423c12a619045cdeaf2445c61220dfac779
ThomasParratt <thomasparratt@googlemail.com>
2026-01-30 10:52:54 +0200

    updated min h

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
             <div className="max-w-4xl mx-auto px-6 space-y-6">
                 <h2 className="text-2xl font-extrabold">ABOUT ME</h2>
                 <article
-                    className="relative min-h-[500px] md:min-h-[600px] border-2 border-orange-600 rounded-2xl p-4 shadow-xl transition bg-amber-100"
+                    className="relative min-h-[500px] md:min-h-[400px] border-2 border-orange-600 rounded-2xl p-4 shadow-xl transition bg-amber-100"
                 >
                     <div>
                         <p className="text-orange-600 font-medium md:text-xl leading-relaxed">

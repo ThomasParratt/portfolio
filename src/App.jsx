0f4f9deea6d533819bc930ca755df1df17d725c9
ThomasParratt <thomasparratt@googlemail.com>
2026-02-02 18:04:45 +0200

    implemented scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,25 @@ import About from './components/About'
 import Footer from './components/Footer'
 import PongGame from "./games/PongGame";
 import PongGameFace from "./games/PongGameFace"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
+import { useEffect } from "react";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-sky-50 text-gray-900">
       <Navbar />
       <main className="flex-grow">
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
